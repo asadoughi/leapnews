072c9227c40259acd46e4da606911c4026071310
Amir Sadoughi <amir.sadoughi@gmail.com>
2014-02-23 13:48:05 -0600

    Simplified code

--- demo10/js/carousel.js
+++ demo10/js/carousel.js
@@ -2,33 +2,16 @@
 /*global document */
 /*global THREE */
 
-var container, stats;
-
-var camera, scene, renderer, target, parent;
-var texture_placeholder;
-var mouseX = 0, mouseY = 0;
-var text, plane;
+var container;
+var camera, scene, renderer, parent;
 var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
-var radious = 1600, theta = 45, onMouseDownTheta = 45, phi = 60;
-var onMouseDownPhi = 60, isShiftDown = false;
+var theta = 45, phi = 60;
 
 function init() {
     container = document.createElement('div');
     document.body.appendChild(container);
-
-    // var info = document.createElement('div');
-    // info.style.position = 'absolute';
-    // info.style.top = '10px';
-    // info.style.width = '100%';
-    // info.style.color = '#ffffff';
-    // info.style.textAlign = 'center';
-    // info.innerHTML = 'Leap News Prototype';
-    // container.appendChild(info);
 
     camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 10,  2500);
     camera.movementSpeed = 100.0;
@@ -41,23 +24,12 @@ function init() {
     parent[0].position.y = 320;
     parent[1].position.y = 220;
     parent[2].position.y = 120;
-    for (var i = 0; i < parent.length; i++) {
-        scene.add(parent[i]);
-
-        // lights
-        var light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(10, 10, 11);
-        light.position.normalize();
-        parent[i].add(light);
-
-        var pointLight = new THREE.PointLight(0xffffff, 0.9);
-        parent[i].add(pointLight);
-    }
+    for (var i = 0; i < parent.length; i++)
+         scene.add(parent[i]);
 
     ////////////////////////////////////////////////////////////////////////
     // Generate 3D Planes in Radius circle
     ////////////////////////////////////////////////////////////////////////
-    var materials = [];
     var arImgRotator = {
         "0": "0342f0984c40b47f86b077ed304b.jpg",
         "1": "04432755468e82b71776d68c38ce.jpg",
@@ -79,117 +51,40 @@ function init() {
     var camSize = 100;
     var startAngle = 0;
     var circleRadius = 230;
-    var centerX = -5;
-    var centerZ = 0.5; // -2.5
 
     var mpi = Math.PI / 180;
-    var startRadians = startAngle + mpi;
+    var startRadians = startAngle * mpi;
     var totalSpheres = 15;
     var incrementAngle = 360 / totalSpheres;
     var incrementRadians = incrementAngle * mpi;
 
     for (var i = 0; i < totalSpheres; i++) {
-        var xp = centerX + Math.sin(startRadians) * circleRadius;
-        var zp = centerZ + Math.cos(startRadians) * circleRadius;
+        var xp = Math.sin(startRadians) * circleRadius;
+        var zp = Math.cos(startRadians) * circleRadius;
         var planObj = new THREE.Mesh(
-            new THREE.PlaneGeometry( camSize, .95*camSize),
-            new THREE.MeshBasicMaterial( {
-                map: THREE.ImageUtils.loadTexture( 'img/'+arImgRotator[i]),
+            new THREE.PlaneGeometry(camSize, 0.95*camSize),
+            new THREE.MeshBasicMaterial({
+                map: THREE.ImageUtils.loadTexture('img/' + arImgRotator[i]),
                 doubleSided: false,
                 wireframe: false,
                 overdraw: true
             }));
           planObj.position.x = xp;
           planObj.position.z = zp;
-          planObj.rotation.y = i*incrementAngle * (Math.PI/180.0); //MH - do this without degrees
+          planObj.rotation.y = i * incrementAngle * mpi;
           startRadians += incrementRadians;
           parent[i%3].add(planObj);
     }
 
-    //////////////////////////////////////////////////////////////////////
-    //  Render
-    //////////////////////////////////////////////////////////////////////
     renderer = new THREE.CanvasRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColorHex(0x3399cc, 0.9);
+    renderer.setClearColorHex(0xF8F8F8, 0.9);
     container.appendChild(renderer.domElement);
-
-    //////////////////////////////////////////////////////////////////////
-    //  addEventListener
-    //////////////////////////////////////////////////////////////////////
-    document.addEventListener('mousedown', onDocumentMouseDown, false);
-    document.addEventListener('touchstart', onDocumentTouchStart, false);
-    document.addEventListener('touchmove', onDocumentTouchMove, false);
-    document.addEventListener('mousewheel', onDocumentMouseWheel, false);
 }
 
-//////////////////////////////////////////////////////////////////////
-//  Mouse events
-//////////////////////////////////////////////////////////////////////
-
-function onDocumentMouseWheel( event ) {
-    camera.position.x =  Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-    camera.position.y =  Math.sin( phi * Math.PI / 360 );
-    camera.position.z =  Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-    camera.updateMatrix();
-    render();
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    camera.lookAt( target );
-}
-
-function onDocumentMouseDown( event ) {
-    event.preventDefault();
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-    mouseXOnMouseDown = event.clientX - windowHalfX;
-    targetRotationOnMouseDown = targetRotation;
-    console.log("targetRotationOnMouseDown: " + targetRotationOnMouseDown);
-}
-
-function onDocumentMouseMove( event ) {
-    mouseX = event.clientX - windowHalfX;
-    mouseY = event.clientY - windowHalfY;
-    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-    console.log("targetRotation: " + targetRotation);
-    camera.position.x =  Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-    render();
-}
-
-function onDocumentMouseUp( event ) {
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-}
-
-function onDocumentMouseOut( event ) {
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-}
-
-function onDocumentTouchStart( event ) {
-    if ( event.touches.length == 1 ) {
-        event.preventDefault();
-        mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotationOnMouseDown = targetRotation;
-    }
-}
-
-function onDocumentTouchMove( event ) {
-    if ( event.touches.length == 1 ) {
-        event.preventDefault();
-        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-    }
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    camera.lookAt( target );
-}
 
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     render();
 }
 
@@ -203,8 +98,8 @@ function render() {
             distance_i = i;
         }
     }
-    parent[distance_i].rotation.y += ( targetRotation - parent[distance_i].rotation.y ) * 0.05;
-    renderer.render( scene, camera );
+    parent[distance_i].rotation.y += (targetRotation - parent[distance_i].rotation.y) * 0.05;
+    renderer.render(scene, camera);
 }
 
 function leapMain() {
@@ -213,6 +108,15 @@ function leapMain() {
     controller.on('frame', function(frameInstance) {
         if (frameInstance.hands.length > 0) {
             for (var i = 0; i < frameInstance.hands.length; i++) {
+                if (frameInstance.hands[i].pointables.length == 1) {
+                    // figure out which image i'm looking at
+                    console.log("bang!");
+                }
+
+                // Looking for open hand preferably, not pointer index
+                if (frameInstance.hands[i].pointables.length < 3)
+                    continue;
+
                 // X-axis - left/right
                 var x_direction = frameInstance.hands[i].palmPosition[0];
                 var percentage = Math.abs(x_direction)/150;
@@ -221,7 +125,7 @@ function leapMain() {
                 } else if (x_direction < -x_threshold) {
                     targetRotation -= 0.02 * percentage;
                 }
-                camera.position.x =  Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+                camera.position.x =  Math.sin(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
 
                 // Y-axis - up/down
                 var y_direction = (frameInstance.hands[i].palmPosition[1]-140);
@@ -236,8 +140,8 @@ function leapMain() {
                 }
 
                 // Z-axis - zoom
-                camera.fov *= (1 + frameInstance.hands[i].palmPosition[2]/50000);
-                camera.updateProjectionMatrix();
+                // camera.fov *= (1 + frameInstance.hands[i].palmPosition[2]/50000);
+                // camera.updateProjectionMatrix();
 
                 render();
             }
