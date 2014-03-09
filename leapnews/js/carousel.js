4f3e821fe4746ab39c93a006077b59e15a2f3ff1
Amir Sadoughi <amir.sadoughi@gmail.com>
2014-03-08 19:30:36 -0600

    Added two more datas

--- leapnews/js/carousel.js
+++ leapnews/js/carousel.js
@@ -105,6 +105,22 @@ data[0].text = "Former National Security Agency contractor Edward Snowden said h
 data[0].description = "Snowden: I raised NSA concerns internally over 10 times before going rogue";
 data[0].tags = "NSA, Security, Surveillance, Snowden"
 
+data[1] = Object();
+data[1].type = "article";
+data[1].url = "http://www.hngn.com/articles/26060/20140307/sxsw-interactive-portion-addresses-nsas-prism-program.htm";
+data[1].image_url = "http://images.hngn.com/data/images/full/17936/eric-schmidt.jpg?w=600";
+data[1].text = "Austin, Texas, is in full-on party mode as South by Southwest kicks off Friday, but the normally lighthearted Interactive portion of the festival is taking a serious turn this year by addressing the National Security Agency's PRISM program head-on, according to Time.com. Reporter Glenn Greenwald, WikiLeaks founder Julian Assange, and NSA whistleblower Edward Snowden will take the stage, Assange and Snowden via a livestream, over the next few days, and even Google executive chairman Eric Schmidt kicked off the festival's first day with deep thoughts on Google's role in the scandal, Time.com reported. SHARE THIS STORY Google chairman Eric Schmidt speaks on the NSA and other topics at South by Southwest on Friday, according to Time.com. 'We were surprised' by the NSA revelations, Schmidt said during a Friday panel at South by Southwest with Jared Cohen, director of Google Ideas, Time.com reported. The agency's work with GCHQ, Britain's surveillance agency, to tap into the fiber-optic networks that carry data between Google's data centers incensed the company, which worked quickly to put the kibosh on the program, called 'Muscular.' 'The very fact that they did this was very suspicious to us,' Schmidt said, Time.com reported. 'The solution to this is to encrypt data at multiple points of source. We now use 2048-bit encryption. We switch the keys at every session. We're pretty sure that any information that's inside of Google is safe from the government's prying eyes, including the U.S. government's.' Schmidt's wariness of the government extends to Snowden, who will also speak at SXSW this weekend, according to Time.com. 'We went to visit with Julian Assange, and both of us felt that who gets to decide what information is public is a pretty fundamental issue in democracy,' Schmidt said, according to Time.com. 'I don't think we want random people leaking large amounts of data. I don't think that serves society.' Schmidt was careful to maintain Google's support of an open Internet free from censorship, Time.com reported. He also criticized countries like Iran that want to lock down the Web and prevent the flow of information, and groups of countries that would band together to participate in the editing of the Internet.";
+data[1].description = "SXSW Interactive Portion Addresses NSA's Prism Program";
+data[1].tags = "Google,Prism,Security,Surveillance,Snowden,SXSW";
+
+data[2] = Object();
+data[2].type = "article";
+data[2].url = "http://www.cnn.com/2014/03/07/politics/nsa-surveillance-extend/";
+data[2].image_url = "http://www.extremetech.com/wp-content/uploads/2013/12/201372421726917734_20.jpg";
+data[2].text = "A federal judge with a secret court has refused the Obama administration's request to extend storage of classified National Security Agency telephone surveillance data beyond the current five-year limit. The Justice Department had argued several pending lawsuits over the bulk data collection program require it to preserve the records for a longer period of time. Judge Reggie Walton, who presides over the Foreign Intelligence Surveillance Court, concluded on Friday the government had not overcome larger privacy concerns. 'The amended procedures would further infringe on the privacy interests of United States persons whose telephone records were acquired in vast numbers and retained by the government to aid in national security investigations,' said Walton, whose main duties are as a Washington-based federal district court judge. 'The great majority of these individuals have never been the subject of investigations by the FBI to protect against international terrorism or clandestine intelligence activities. The government seeks to retain these records, not for national security reasons, but because some of them may be relevant in civil litigation in which the destruction of those very same records is being requested. However, the civil plaintiffs potentially interested in preserving the (telecom) metadata have expressed no desire to acquire the records.' Current surveillance court orders require the National Security Agency or telecommunication companies that gathered the phone records to purge the material within five years. 'The government makes no attempt to explain why it believes the records that are subject to destruction are relevant to the civil cases,' said Walton in his 12-page order. There was no immediate reaction to the order from the Justice Department. Intellgence leaker Edward Snowden last June revealed a secret surveillance court order approving government collection of mass amounts of metadata from telecom giant Verizon and leading Internet companies, including Microsoft, Apple, Google, Yahoo and Facebook. It includes phone numbers called and their location. The exact percentage of metadata being collected has not been revealed publicly. Monitoring of actual conversations requires a separate warrant. President Barack Obama in January cited privacy concerns when announcing that such data should no longer be held by the government, but instead be turned over to the domestic telecoms or a private third party. NSA would still have access to the calls to track potential terror connections. He ordered the intelligence community to formulate such a plan by March 28. The mission of the FISA court, named after the Foreign Intelligence Surveillance Act that created it, is to decide whether to grant certain types of government requests -- wiretapping, data analysis, and other monitoring for 'foreign intelligence purposes' of suspected terrorists and spies operating in the United States. The once-secret approval of collecting bits and pieces of information from electronic communications comes quarterly from judges at the court. To collect the information, the government has to demonstrate to a judge that it is 'relevant' to an international terrorism investigation. There were 1,856 applications in 2012 to the FISA Court for electronic surveillance and physical searches for 'foreign intelligence purposes,' the Justice Department said. The current case is In re: Application of the FBI for an Order Requiring the Production of Tangible Things (BR 14-01).";
+data[2].description = "Government can't hold NSA surveillance data longer";
+data[2].tags = "NSA,Obama,Surveillance,Data,Security,Rights";
+
     var camSize = 100;
     var startAngle = 0;
     var circleRadius = 230;
@@ -116,20 +132,21 @@ data[0].tags = "NSA, Security, Surveillance, Snowden"
     var incrementRadians = incrementAngle * mpi;
 
     for (var i = 0; i < totalSpheres; i++) {
+        var f = i;
         var xp = Math.sin(startRadians) * circleRadius;
         var zp = Math.cos(startRadians) * circleRadius;
         var planObj = new THREE.Mesh(
             new THREE.PlaneGeometry(camSize, 0.95*camSize),
             new THREE.MeshBasicMaterial({
-                map: THREE.ImageUtils.loadTexture(data[0].image_url),
+                map: THREE.ImageUtils.loadTexture(data[f%3].image_url),
                 doubleSided: false,
                 wireframe: false,
                 overdraw: true
             }));
-        planObj.name = data[0].image_url
-        planObj.title = data[0].description
-        planObj.gallery = split_text(data[0].text, 200)
-        planObj.tags = render_tags(data[0].tags);
+        planObj.name = data[f%3].image_url
+        planObj.title = data[f%3].description
+        planObj.gallery = split_text(data[f%3].text, 200)
+        planObj.tags = render_tags(data[f%3].tags);
 
         planObj.position.x = xp;
         planObj.position.z = zp;
